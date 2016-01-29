28d69e2b878b2c5d20c2c1a280c80e63398cebe6
hieubkcse <huynhtrunghieu.cs@gmail.com>
2016-01-29 12:31:51 +0700

    update lab1

--- Lab1/Lab1-CG/Lab1-CG/Exercise1.cpp
+++ Lab1/Lab1-CG/Lab1-CG/Exercise1.cpp
@@ -1,34 +1,43 @@
 #include <GL/glut.h>
 #include <math.h>
 #include <stdio.h>
+GLfloat offset;
 #define DEG2RAD (3.14159f/180.0f)
+
+void drawHalfCircle(double numberA, int numberB) {
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glLineWidth(5);
+	glBegin(GL_LINE_STRIP);
+	for (int i = 0; i < numberB; i++) {
+		glVertex2f(numberA*cos(DEG2RAD * 1 * i), numberA*sin(DEG2RAD * 1 * i));
+	}
+	glEnd();
+}
 
 void mydisplay() {
 	glClear(GL_COLOR_BUFFER_BIT);
-
-	glColor3f(1.0f, 1.0f, 0.0f);
-	glBegin(GL_LINE_LOOP);
-	for (int i = 0; i < 36; i++) {
-		glVertex2f(0.75*cos(DEG2RAD * 10 * i), 0.75*sin(DEG2RAD * 10 * i));
+	for (double j = 0; j < 0.7; j = j + 0.05) {
+		drawHalfCircle(j, 181);
 	}
-	glEnd();
-
-	glColor3f(1.0f, 0.0f, 0.0f);
-	glPointSize(5);
-
-	glBegin(GL_POINTS);
-	for (int i = 0; i < 36; i++) {
-		glVertex2f(0.75*cos(DEG2RAD * 10 * i), 0.75*sin(DEG2RAD * 10 * i));
-	}
-	glEnd();
-
+	
 	glFlush();
 	glutSwapBuffers();
 }
 
+void processTimer(int value) {
+	offset += (GLfloat)value;
+	
+	if (offset > 360) offset = offset - 360.0f;
+
+	glutTimerFunc(15, processTimer, value);
+	glutPostRedisplay();
+}
+
 int main(int argc, char** argv) {
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(500, 500);
 	glutCreateWindow("simple");
 	glutDisplayFunc(mydisplay);
+	glutTimerFunc(50, processTimer, 10);
 	glutMainLoop();
 }
